19b86c924501b54b03165b32e314fe028fa21bc7
Guillaume <guillaume@dinmo.com>
2023-09-21 15:02:24 +0200

    fix: upgrade typescript

--- packages/quicktype-typescript-input/src/index.ts
+++ packages/quicktype-typescript-input/src/index.ts
@@ -2,6 +2,7 @@ import * as ts from "typescript";
 import { PartialArgs, generateSchema } from "@mark.probst/typescript-json-schema";
 
 import { defined, JSONSchemaSourceData, messageError } from "quicktype-core";
+import * as path from 'path';
 
 const settings: PartialArgs = {
     required: true,
@@ -11,20 +12,78 @@ const settings: PartialArgs = {
 };
 
 const compilerOptions: ts.CompilerOptions = {
-    noEmit: true,
-    emitDecoratorMetadata: true,
-    experimentalDecorators: true,
-    target: ts.ScriptTarget.ES5,
-    module: ts.ModuleKind.CommonJS,
-    strictNullChecks: true,
-    typeRoots: [],
-    rootDir: "."
+    "module": ts.ModuleKind.CommonJS,
+    "declaration": true,
+    "allowImportingTsExtensions": true,
+    "removeComments": true,
+    "emitDecoratorMetadata": true,
+    "experimentalDecorators": true,
+    "allowSyntheticDefaultImports": true,
+    "target": ts.ScriptTarget.ES2020,
+    "sourceMap": true,
+    "outDir": "./dist",
+    "skipLibCheck": true,
+    "strictNullChecks": false,
+    "noImplicitAny": false,
+    "strictBindCallApply": false,
+    "forceConsistentCasingInFileNames": false,
+    "noFallthroughCasesInSwitch": false,
+    "esModuleInterop": true,
+    "strict": true,
+    "lib": ["lib.es2020.d.ts", "lib.es2020.bigint.d.ts"],
+    "typeRoots": ["node_modules/@types"]
 };
+
+function getTsConfig() {
+
+    const currentDirectory = process.cwd();
+
+    const tsConfigPath = ts.findConfigFile(
+        currentDirectory,
+        ts.sys.fileExists,
+        'tsconfig.json'
+    );
+
+    if (tsConfigPath) {
+        console.log(`Found tsconfig.json at: ${tsConfigPath}`);
+
+        // Read the tsconfig.json file
+        const configFile = ts.readConfigFile(tsConfigPath, ts.sys.readFile);
+
+        if (configFile.error) {
+            console.error("Error reading tsconfig.json:", ts.flattenDiagnosticMessageText(configFile.error.messageText, "\n"));
+            return;
+        }
+
+        // Parse the configuration
+        const basePath = path.dirname(tsConfigPath); // Directory of tsconfig.json
+        const config = ts.parseJsonConfigFileContent(configFile.config, ts.sys, basePath);
+
+        if (config.errors && config.errors.length > 0) {
+            console.error("Error parsing tsconfig.json:");
+            config.errors.forEach(error => {
+                console.error(ts.flattenDiagnosticMessageText(error.messageText, "\n"));
+            });
+            return;
+        } else {
+            return config;
+        }
+
+        console.log("Parsed tsconfig.json:", config);
+
+    } else {
+        console.log('No tsconfig.json found in the directory tree.');
+    }
+}
 
 // FIXME: We're stringifying and then parsing this schema again.  Just pass around
 // the schema directly.
 export function schemaForTypeScriptSources(sourceFileNames: string[]): JSONSchemaSourceData {
-    const program = ts.createProgram(sourceFileNames, compilerOptions);
+    const config = getTsConfig()
+    if (!config) {
+        throw new Error('ts config file not found');
+    }
+    const program = ts.createProgram(sourceFileNames, config.options || compilerOptions);
     const diagnostics = ts.getPreEmitDiagnostics(program);
     const error = diagnostics.find(d => d.category === ts.DiagnosticCategory.Error);
     if (error !== undefined) {
